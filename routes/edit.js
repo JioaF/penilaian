bef64069a028ff1d4031264f502af5fc96115220
JioaF <70670116+JioaF@users.noreply.github.com>
2021-01-20 13:04:05 +0700

    update

--- routes/edit.js
+++ routes/edit.js
@@ -6,26 +6,13 @@ var conn = require('../controller/database');
 router.get('/:id',function(req, res){
     var getid = req.params.id;
 
-    var data = {
-        nis : req.body.nis,
-        nama_lengkap : req.body.nama,
-        kelas : req.body.kelas,
-        jurusan : req.body.jurusan,
-        tempat_lahir : req.body.tempatl,
-        tanggal_lahir : req.body.tgll,
-        alamat : req.body.alamat,
-        email : req.body.email,
-        no_telp : req.body.notelp
-    };
+   var data = {};
     
-    conn.query('SELECT FROM tab_siswa WHERE id = ?', [getid], function(err, results, fields){
+    conn.query('SELECT * FROM tab_siswa WHERE id = ?', [getid], function(err, results, fields){
         if(err) throw err;
-        if(results.length > 0){
-            res.render('edit_page', {text : "Edit data Siswa", id:getid}) 
-        }else{
-            res.send("Data not found!");
-        }
-          
+        
+            
+        res.render('edit_page', {text:"Edit data  Siswa", data:results});          
     });
 })
 
